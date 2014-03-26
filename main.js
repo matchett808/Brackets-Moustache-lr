e6d3a7783848e02bb22fba75176756245df110a6
Iain Matchett <iainmatchett@gmail.com>
2014-03-26 09:32:06 +0000

    Initial commit with a small amount of stuff, Readme, pckg file and bare main.js for brackets

---
+++ main.js
@@ -0,0 +1,13 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window, Mustache */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    var AppInit = brackets.getModule("utils/AppInit");
+
+
+    AppInit.appReady(function () {
+        //...
+    });
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+#Moustach-lr
+
+I've been using Moustache in several contexts recently, I've found that being able to write moustache templates and contexts with an auto-compile function in Brackets would be useful....So I'm making a Brackets extension...
+
+
+##**THIS IS A WORK IN PROGRESS AS OF TODAY (26/03/2014) THIS IS INCOMPLETE!**##
