81bd7468f4edffd190d841c3201e29416b727c8e
iain matchett <iainmatchett@gmail.com>
2014-03-26 19:16:40 +0000

    Finally got the file switches working,  - console log messages still in place - should be removed for next commit...

--- main.js
+++ main.js
@@ -3,36 +3,46 @@
 
 define(function (require, exports, module) {
     "use strict";
-    var AppInit = brackets.getModule("utils/AppInit");
-    //var CommandManager = brackets.getModule("command/CommandManager");
-    //var Menus          = brackets.getModule("command/Menus");
-    //var EditorManager  = brackets.getModule("editor/EditorManager");
-    //var ProjectManager = brackets.getModule("project/ProjectManager");
-    var FileUtils = brackets.getModule("file/FileUtils");    
-    //var NativeApp = brackets.getModule("utils/NativeApp");
-    //var Commands = brackets.getModule("command/Commands");
-         function onDocumentSaved(event, doc) {
-      var DocumentManager = brackets.getModule("document/DocumentManager");    
-             
-             var fname = DocumentManager.getCurrentDocument().file._name;
-             // if current file == *.moustache
-             if(fname.match('.*?\.moustache'))
-             {
-                 //find current path
-                 var pname = DocumentManager.getCurrentDocument().file._path;
-                 var file = FileUtils.getDirectoryPath(pname) + "project.json";
-                 if (_openDocuments[file.fullPath]) {
-                 }
-                 //if current folder contains project.json && project.context
-                    // read project.json
-                    // if project.json => compile == true
-                        // set output file based on project.json
-                        // read project.context
-                        // pass project.context & currentfile -> moustache...
-             }
-        }
+var AppInit = brackets.getModule("utils/AppInit");
+//var CommandManager = brackets.getModule("command/CommandManager");
+//var Menus          = brackets.getModule("command/Menus");
+//var EditorManager  = brackets.getModule("editor/EditorManager");
+var ProjectManager = brackets.getModule("project/ProjectManager");
+var FileUtils = brackets.getModule("file/FileUtils");    
+var  FileSystem = brackets.getModule("filesystem/FileSystem");
+//var NativeApp = brackets.getModule("utils/NativeApp");
+//var Commands = brackets.getModule("command/Commands");
+var NativeFileSystem    = brackets.getModule("file/NativeFileSystem").NativeFileSystem;
+var DocumentManager = brackets.getModule("document/DocumentManager");    
+    
+    
+    
+ function onDocumentSaved(event, doc) {
+     var fname = DocumentManager.getCurrentDocument().file._name;
+     // if current file == *.moustache
+     if(fname.match('.*?\.moustache') || true)
+     {
+        //find current path
+        var pname = DocumentManager.getCurrentDocument().file._path;
+        var project_file = FileUtils.getDirectoryPath(pname) + "project.json";
+        var context_file = FileUtils.getDirectoryPath(pname) + "context.json";
+        var projectRoot = ProjectManager.getProjectRoot();
+        console.log("pre-if");
+         //if current folder contains project.json && project.context
+         if (
+             FileSystem.getFileForPath(project_file)._stat != null && 
+             FileSystem.getFileForPath(context_file)._stat !=null
+            ){
+             console.log("payload");
+            // read project.json
+            // if project.json => compile == true
+                // set output file based on project.json
+                // read project.context
+                // pass project.context & currentfile -> moustache...
+         }
+     }
+}
     AppInit.appReady(function () {
-      var DocumentManager = brackets.getModule("document/DocumentManager");    
         $(DocumentManager).on('documentSaved', onDocumentSaved);
     });
 });
