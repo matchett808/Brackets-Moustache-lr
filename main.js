1a1501c23fabd5373989e62b2764456e90dd8159
Iain Matchett <iainmatchett@gmail.com>
2014-03-26 09:53:15 +0000

    Commit of some changes to main.js - nothing works still - just started :)

--- main.js
+++ main.js
@@ -3,11 +3,24 @@
 
 define(function (require, exports, module) {
     "use strict";
-
     var AppInit = brackets.getModule("utils/AppInit");
-
+    var CommandManager = brackets.getModule("command/CommandManager");
+    var Menus          = brackets.getModule("command/Menus");
+    //var EditorManager  = brackets.getModule("editor/EditorManager");
+    //var ProjectManager = brackets.getModule("project/ProjectManager");
+    //var FileUtils = brackets.getModule("file/FileUtils");    
+    var DocumentManager = brackets.getModule("document/DocumentManager");    
+    //var NativeApp = brackets.getModule("utils/NativeApp");
+    //var Commands = brackets.getModule("command/Commands");
+         function _onDocumentSaved(event, doc) {
+            alert("magic");
+        }
 
     AppInit.appReady(function () {
-        //...
     });
 });
+
+
+
+
+  
